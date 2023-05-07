4b2282abaa2009a6a03e0677e0f83dfe3fe03106
Kaizne <kaizne@kaizne.com>
2023-05-07 18:07:52 -0400

    Replace id with slug

--- src/api/quiz/controllers/quiz.ts
+++ src/api/quiz/controllers/quiz.ts
@@ -2,6 +2,17 @@
  * quiz controller
  */
 
-import { factories } from '@strapi/strapi'
+const { createCoreController } = require('@strapi/strapi').factories
 
-export default factories.createCoreController('api::quiz.quiz');
+module.exports = createCoreController('api::quiz.quiz', ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id: slug } = ctx.params
+      const { query } = ctx
+      if (!query.filters) query.filters = {}
+      query.filters.slug = { '$eq': slug }
+      const entity = await strapi.service('api::quiz.quiz').find(query)
+      const { results } = await this.sanitizeOutput(entity, ctx)
+      return this.transformResponse(results[0])
+    }
+  })
+)
