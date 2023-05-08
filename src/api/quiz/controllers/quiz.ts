4d39744408836e11a02fb0c6cebd5d687a63f08f
Kaizne <kaizne@kaizne.com>
2023-05-07 23:46:02 -0400

    Populate images

--- src/api/quiz/controllers/quiz.ts
+++ src/api/quiz/controllers/quiz.ts
@@ -6,11 +6,19 @@ const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::quiz.quiz', ({ strapi }) =>  ({
     async findOne(ctx) {
-        const { slug } = ctx.params;
+        const { slug } = ctx.params
         const entity = await strapi.db.query('api::quiz.quiz').findOne({
-          where: { slug }
+          where: { slug },
+          populate: {
+            media: true,
+            questions: {
+                populate: {
+                    media: true
+                }   
+            }
+          }
         })
-        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-        return this.transformResponse(sanitizedEntity);
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
+        return this.transformResponse(sanitizedEntity)
       }
 }))
